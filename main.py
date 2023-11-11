795e5817a9d4546bcdac3e71158b573a63656809
Adrian <reinieradrianluna@gmail.com>
2023-11-11 15:03:10 +0800

    Themes and Color palette

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton, MDIconButton
+from kivymd.uix.button import MDRectangleFlatButton
 
 class reportApp(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.primary_hue = "A700"
+        self.theme_cls.theme_style = "Dark"
         screen = Screen()
         btn_flat = MDRectangleFlatButton(text = 'Hello World',pos_hint= {'center_x':0.5, 'center_y':0.5})
-        icon_btn = MDIconButton(icon='android', pos_hint= {'center_x':0.5, 'center_y':0.5} )
-        screen.add_widget(icon_btn)
+        screen.add_widget(btn_flat)
         return screen
    
 reportApp().run()
