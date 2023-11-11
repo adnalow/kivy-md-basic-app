6c7a5570a4b0cdec51a6183d7bcc39c3645e9b6d
Adrian <reinieradrianluna@gmail.com>
2023-11-11 14:37:55 +0800

    labels and text_styles

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel,MDIcon
 
 class reportApp(MDApp):
     def build(self):
-        return
-    
+        label = MDLabel(text='Hello World', halign = 'center', theme_text_color = 'Custom', text_color=(236/255.0,98/255.0,81/255.0,1), font_style = 'Caption')
+        
+        icon_label = MDIcon(icon = 'library-video', halign = 'center')
+        return icon_label
+   
 reportApp().run()
