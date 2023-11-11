624329db31b8faa349ae66e756c52f94a7275321
Adrian <reinieradrianluna@gmail.com>
2023-11-11 14:46:29 +0800

    button creation

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel,MDIcon
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton, MDIconButton
 
 class reportApp(MDApp):
     def build(self):
-        label = MDLabel(text='Hello World', halign = 'center', theme_text_color = 'Custom', text_color=(236/255.0,98/255.0,81/255.0,1), font_style = 'Caption')
-        
-        icon_label = MDIcon(icon = 'library-video', halign = 'center')
-        return icon_label
+        screen = Screen()
+        btn_flat = MDRectangleFlatButton(text = 'Hello World',pos_hint= {'center_x':0.5, 'center_y':0.5})
+        icon_btn = MDIconButton(icon='android', pos_hint= {'center_x':0.5, 'center_y':0.5} )
+        screen.add_widget(icon_btn)
+        return screen
    
 reportApp().run()
